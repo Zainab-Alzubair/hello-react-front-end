3f89c35e161088e7046c53a1e9899739b9f0dba6
Zainab-Alzubair <zainab.a.elzubaie@gmail.com>
2023-05-06 20:29:02 +0300

    Change the port

--- src/redux/greetingSlice.js
+++ src/redux/greetingSlice.js
@@ -5,7 +5,7 @@ const FETCH_GREETING = 'HELLO_WORLD/FETCH_DATA';
 export const fetchGreeting = createAsyncThunk(
   FETCH_GREETING,
   async () => {
-    const response = await fetch('http://127.0.0.1:1800/api/v1/greetings');
+    const response = await fetch('http://127.0.0.1:3000/api/v1/greetings');
     const data = await response.json();
     return data;
   },
